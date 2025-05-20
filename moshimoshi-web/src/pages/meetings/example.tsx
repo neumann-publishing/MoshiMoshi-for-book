dfb6148381d3837cc1bde9c432644bc92fe8202c
kbaba1001 <kbaba1001@users.noreply.github.com>
2025-05-20 20:12:20 +0900

    feat: select video devices

--- moshimoshi-web/src/pages/meetings/example.tsx
+++ moshimoshi-web/src/pages/meetings/example.tsx
@@ -1,40 +1,71 @@
-import { useEffect, useRef } from "react";
+import { Flex, Radio, RadioGroup } from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+import { DeviceType, getDevices } from "../../libs/devices";
 
 export function MeetingsExamplePage() {
 	const videoRef = useRef<HTMLVideoElement>(null);
+	const [videoDevices, setVideoDevices] = useState<MediaDeviceInfo[]>([]);
+	const [currentVideoDeviceId, setCurrentVideoDeviceId] = useState<string>();
+	const [stream, setStream] = useState<MediaStream | null>(null);
 
 	useEffect(() => {
-		let stream: MediaStream | null = null;
+		const getVideoDevices = async () => {
+			const devices = await getDevices(DeviceType.camera);
+			setVideoDevices(devices);
+			setCurrentVideoDeviceId(devices[0]?.deviceId);
+		};
+		getVideoDevices();
+	}, []);
 
+	useEffect(() => {
 		const getMedia = async () => {
-			stream = await navigator.mediaDevices.getUserMedia({
-				video: true,
+			const stream = await navigator.mediaDevices.getUserMedia({
+				video: {
+					deviceId: currentVideoDeviceId
+						? { exact: currentVideoDeviceId }
+						: undefined,
+				},
 				audio: true,
 			});
+			setStream(stream);
 			if (videoRef.current) {
 				videoRef.current.srcObject = stream;
 			}
 		};
-
 		getMedia();
+	}, [currentVideoDeviceId]);
 
+	useEffect(() => {
 		return () => {
-			// コンポーネントアンマウント時にストリームを停止
 			if (stream) {
 				for (const track of stream.getTracks()) {
 					track.stop();
 				}
 			}
 		};
-	}, []);
+	}, [stream]);
 
 	return (
-		<video
-			ref={videoRef}
-			style={{ width: "480px", height: "360px", background: "#000" }}
-			autoPlay
-			playsInline
-			muted
-		/>
+		<Flex flexDirection="column">
+			<video
+				ref={videoRef}
+				style={{ width: "480px", height: "360px", background: "#000" }}
+				autoPlay
+				playsInline
+				muted
+			/>
+			<RadioGroup
+				value={currentVideoDeviceId}
+				onChange={(value) => setCurrentVideoDeviceId(value)}
+			>
+				<Flex flexDirection="column">
+					{videoDevices.map((device) => (
+						<Radio key={device.deviceId} value={device.deviceId}>
+							{device.label}
+						</Radio>
+					))}
+				</Flex>
+			</RadioGroup>
+		</Flex>
 	);
 }
